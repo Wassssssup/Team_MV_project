193998af5781f7187791051e37af1d0be15579b4
kimDaBean <kimDaBean@KIM-PC>
2022-10-25 23:09:02 +0900

    2022-10-25 Frame S

---
+++ src/main/java/kr/co/ezenac/mapper/TopMenuMapper.java
@@ -0,0 +1,6 @@
+package kr.co.ezenac.mapper;
+
+//쿼리문(마이바티스) 부분
+public interface TopMenuMapper {
+	
+}
---
+++ src/main/java/kr/co/ezenac/service/TopMenuService.java
@@ -0,0 +1,9 @@
+package kr.co.ezenac.service;
+
+import org.springframework.stereotype.Service;
+
+//Service >> DB 연결 후 처리하는 곳
+@Service
+public class TopMenuService {
+	
+}
